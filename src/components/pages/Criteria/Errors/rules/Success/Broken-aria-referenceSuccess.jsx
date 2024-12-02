38b89745b3115217754132e1fa6fae2cd421cddc
Jason Quaicoo <jayquake@gmail.com>
2024-12-02 15:05:38 +0200

    updated broken aria reference

--- src/components/pages/Criteria/Errors/rules/Success/Broken-aria-referenceSuccess.jsx
+++ src/components/pages/Criteria/Errors/rules/Success/Broken-aria-referenceSuccess.jsx
@@ -7,37 +7,32 @@ export default () => (
   <IssueSuccess
     itemContent={
       <>
-        <div className="list-item" id="brokenAriaReference-success-1">
-          <p>Input field with valid aria-labelledby pointing to an existing label.</p>
+        <div className="list-item" id="ariaReference-success-1">
           <label id="name-label" htmlFor="name-input">Name</label>
           <input id="name-input" type="text" aria-labelledby="name-label" />
         </div>
         
-        <div className="list-item" id="brokenAriaReference-success-2">
-          <p>Input field with valid aria-describedby pointing to helper text.</p>
-          <p id="password-instructions">Password must be 8-20 characters long.</p>
+        <div className="list-item" id="ariaReference-success-2">
+          <p id="password-instructions">Password must be at least 8 characters long.</p>
           <input id="password-input" type="password" aria-describedby="password-instructions" />
         </div>
         
-        <div className="list-item" id="brokenAriaReference-success-3">
-          <p>Button with aria-controls pointing to a valid menu element.</p>
+        <div className="list-item" id="ariaReference-success-3">
           <button aria-controls="dropdown-menu" aria-expanded="false">Toggle Menu</button>
           <ul id="dropdown-menu" hidden>
-            <li><a href="#">Option 1</a></li>
-            <li><a href="#">Option 2</a></li>
+            <li>Option 1</li>
+            <li>Option 2</li>
           </ul>
         </div>
         
-        <div className="list-item" id="brokenAriaReference-success-4">
-          <p>Listbox with aria-owns referencing valid option elements.</p>
+        <div className="list-item" id="ariaReference-success-4">
           <div role="listbox" id="listbox" aria-owns="option1 option2">
             <div id="option1" role="option">Option 1</div>
             <div id="option2" role="option">Option 2</div>
           </div>
         </div>
         
-        <div className="list-item" id="brokenAriaReference-success-5">
-          <p>Autocomplete input with aria-activedescendant referencing a valid suggestion.</p>
+        <div className="list-item" id="ariaReference-success-5">
           <ul id="autocomplete-list" role="listbox">
             <li id="suggestion1" role="option">Suggestion 1</li>
             <li id="suggestion2" role="option">Suggestion 2</li>
@@ -45,8 +40,7 @@ export default () => (
           <input type="text" aria-activedescendant="suggestion1" aria-owns="autocomplete-list" />
         </div>
         
-        <div className="list-item" id="brokenAriaReference-success-6">
-          <p>Search input field with aria-labelledby referencing a valid label.</p>
+        <div className="list-item" id="ariaReference-success-6">
           <input id="search-input" type="search" aria-labelledby="search-label" />
           <label id="search-label" htmlFor="search-input">Search</label>
         </div>
