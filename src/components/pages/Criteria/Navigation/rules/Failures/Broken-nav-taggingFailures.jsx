452817ee9c741134994e8255c827e3aa1414fd59
Jason Quaicoo <jayquake@gmail.com>
2025-01-01 18:42:04 +0200

    Broken Nav2

--- src/components/pages/Criteria/Navigation/rules/Failures/Broken-nav-taggingFailures.jsx
+++ src/components/pages/Criteria/Navigation/rules/Failures/Broken-nav-taggingFailures.jsx
@@ -7,6 +7,7 @@ export default () => (
   <IssueFailure
     itemContent={
       <>
+        {/* Failure Example 1 */}
         <div className="list-item" id="menu-avoid-failure-1">
           <nav role="menu">
             <ul>
@@ -15,6 +16,8 @@ export default () => (
             </ul>
           </nav>
         </div>
+
+        {/* Failure Example 2 */}
         <div className="list-item" id="menu-avoid-failure-2">
           <header>
             <nav role="menu" aria-label="Header Menu">
@@ -23,6 +26,8 @@ export default () => (
             </nav>
           </header>
         </div>
+
+        {/* Failure Example 3 */}
         <div className="list-item" id="menu-avoid-failure-3">
           <footer>
             <nav role="menu">
@@ -31,6 +36,8 @@ export default () => (
             </nav>
           </footer>
         </div>
+
+        {/* Failure Example 4 */}
         <div className="list-item" id="menu-bar-avoid-failure-4">
           <nav role="menubar" aria-label="Main Menubar">
             <ul>
@@ -39,6 +46,8 @@ export default () => (
             </ul>
           </nav>
         </div>
+
+        {/* Failure Example 5 */}
         <div className="list-item" id="menu-bar-avoid-failure-5">
           <nav role="menubar">
             <ul>
@@ -47,6 +56,8 @@ export default () => (
             </ul>
           </nav>
         </div>
+
+        {/* Failure Example 6 */}
         <div className="list-item" id="menu-bar-avoid-failure-6">
           <nav role="menubar" aria-label="Footer Menubar">
             <ul>
@@ -54,6 +65,61 @@ export default () => (
               <li><a href="#support">Support</a></li>
             </ul>
           </nav>
+        </div>
+
+        {/* Failure Example 7 */}
+        <div className="list-item" id="menu-bar-avoid-failure-7">
+          <div role="menu" aria-label="Main navigation">
+            <div role="menuitem" aria-haspopup="true" tabindex="0" aria-expanded="false" id="fileMenuButton" onclick="toggleMenu('fileMenu')">File</div>
+            <div id="fileMenu" role="menu" aria-labelledby="fileMenuButton" style={{ display: "none" }}>
+              <div role="menuitem" tabindex="-1" onclick="newFile()">New</div>
+              <div role="menuitem" tabindex="-1" onclick="openFile()">Open</div>
+              <div role="menuitem" tabindex="-1" onclick="saveFile()">Save</div>
+            </div>
+          </div>
+        </div>
+
+        {/* Failure Example 8 */}
+        <div className="list-item" id="menu-bar-avoid-failure-8">
+          <ul role="menu" aria-label="Main Menu">
+            <li role="menuitem"><a href="#home">Home</a></li>
+            <li role="menuitem"><a href="#about">About</a></li>
+          </ul>
+        </div>
+
+        {/* Failure Example 9 */}
+        <div className="list-item" id="menu-bar-avoid-failure-9">
+          <div role="menubar" aria-label="Navigation">
+            <button role="menuitem">Dashboard</button>
+            <button role="menuitem">Reports</button>
+            <button role="menuitem">Settings</button>
+          </div>
+        </div>
+
+        {/* Failure Example 10 */}
+        <div className="list-item" id="menu-bar-avoid-failure-10">
+          <section role="menu" aria-label="Sidebar Menu">
+            <div role="menuitem" tabindex="0">Overview</div>
+            <div role="menuitem" tabindex="0">Details</div>
+          </section>
+        </div>
+
+        {/* Failure Example 11 */}
+        <div className="list-item" id="menu-bar-avoid-failure-11">
+          <footer role="menu" aria-label="Footer Links">
+            <div role="menuitem"><a href="#privacy">Privacy Policy</a></div>
+            <div role="menuitem"><a href="#terms">Terms of Service</a></div>
+          </footer>
+        </div>
+
+        {/* Failure Example 12 */}
+        <div className="list-item" id="menu-bar-avoid-failure-12">
+          <aside role="menu" aria-label="Side Navigation">
+            <nav role="menubar">
+              <a role="menuitem" href="#help">Help</a>
+              <a role="menuitem" href="#support">Support</a>
+            </nav>
+          </aside>
         </div>
       </>
     }
