64ff80b6168509fc8cb7aab15a2809ace4d1297f
Jason Quaicoo <jayquake@gmail.com>
2025-01-27 13:18:19 +0200

    Enhance accessibility by adding label elements for empty headings and improving heading structure added Customer portal example

--- src/components/pages/Criteria/Headings/rules/Failures/Empty-headingsFailure.jsx
+++ src/components/pages/Criteria/Headings/rules/Failures/Empty-headingsFailure.jsx
@@ -43,6 +43,37 @@ export default () => (
         <div className="list-item" id="empty-headings-failure-6">
           <h4 aria-level="5">Untitled</h4>
         </div>
+        <div className="list-item" id="empty-headings-labelfor-failure-1">
+          <label for="emptyheading-labelfor-failure-1">heading text using for attribute h5</label
+          ><h5 id="emptyheading-labelfor-failure-1"></h5>
+        </div>
+        <div className="list-item" id="empty-headings-labelfor-failure-2">
+          <label for="emptyheading-labelfor-failure-2">heading text using for attribute h6</label
+          ><h6 id="emptyheading-labelfor-failure-2"> </h6>
+        </div>
+        <div className="list-item" id="empty-headings-labelfor-failure-3">
+          <label for="emptyheading-labelfor-failure-3">heading text using for attribute h1</label
+          ><h1 id="emptyheading-labelfor-failure-3">&nbsp;</h1>
+        </div>
+        <div className="list-item" id="empty-headings-labelfor-failure-4">
+          <label for="emptyheading-labelfor-failure-4">heading text using for attribute h2</label
+          ><h2 id="emptyheading-labelfor-failure-4">No Title</h2>
+        </div>
+      <div className="list-item" id="empty-headings-labelfor-failure-5">
+        <label for="emptyheading-labelfor-failure-5">
+          Dashborard/ Customer Portal Label for empty heading element example
+        </label>
+        <a scale="base" id="emptyheading-labelfor-failure-5" className="sc-cGXZpB bPwspU sc-jXErnb JtxYj override" href="/app/widget-license/619a932d8dbc2f0444b3ed3d">
+          <span>
+            <span data-mixpanel="buy-license-announcement-bar"> example behavior</span>
+          </span>
+          <i aria-hidden="true" role="presentation" size="14" className="sc-ldgYGE gMHHrU">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="m8 4 8 8-8 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+          </i>
+        </a>
+      </div>
       </>
     }
     itemDescription={itemDescription}
