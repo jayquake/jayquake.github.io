6473b50f339ff5586c569173027a4296ac08fd86
jasonqua <jasonqu@accessibe.com>
2025-11-11 13:09:10 +0200

    Deploy: Trigger GitHub Actions deployment

--- src/components/pages/Criteria/Tables/tables.jsx
+++ src/components/pages/Criteria/Tables/tables.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import List from "../../listRules";
 
 const Tables = () => {
-  const filters = [{ property: "criteria", value: "tables" }];
+const filters = [{ property: "criteria", value: "tables" }];
   return <List filters={filters} embedded={true} />;
 };
 
